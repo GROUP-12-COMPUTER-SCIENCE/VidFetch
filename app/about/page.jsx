83a85a20af074118ea2b21a41ead25597fce506b
CHEGEBB <chegephil24@gmail.com>
2024-06-20 20:35:12 +0300

    added styles for logo

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <main>
+        <h1>About Us</h1>
+        <p>This is the about us page</p>
+    </main>
+  )
+}
---
+++ app/contact/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <main>
+        <h1>Contact Us</h1>
+        <p>This is the contact us page</p>
+    </main>
+  )
+}

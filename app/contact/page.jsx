0440808ecd75e4e9c0295de153e35a8855417be9
CHEGEBB <chegephil24@gmail.com>
2024-06-21 10:40:56 +0300

    added contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import styles from "./contact.scss";
+import Navbar from "../components/Navbar";
 
 export default function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
   return (
-    <main>
+    <main className={styles.main}>
+      <Navbar />
+      <div className={styles.content}>
         <h1>Contact Us</h1>
-        <p>This is the contact us page</p>
+        <h2>
+          <small>We would love to hear from you! Fill out the form below to get in touch with us.</small>
+        </h2>
+        <div className="frosted">
+          <form >
+            <input type="text" name="name" placeholder="Your Name" required />
+            <input type="email" name="email" placeholder="Your Email" required />
+            <textarea name="message" placeholder="Your Message" required></textarea>
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -8,76 +8,83 @@ import Image from "next/image";
 import Logo from "./components/images/line-md--cloud-download-loop.svg";
 
 export default function Home() {
-  const handleSubmit=() =>{
-    console.log("Form submitted")
-  }
+  const handleSubmit = () => {
+    console.log("Form submitted");
+  };
   return (
     <main className={styles.main}>
-    <Navbar/>
-    <div className="content">
-    <div className="logo">
-      <Image
-      src={Logo}
-      alt="VidFetch Logo"
-      width={60}
-      quality={100}
-      />
-      <h1>VidFetch</h1>
-    </div>
-    <h1>VidFetch - YouTube Video Downloader</h1>
-    <h2><small>Download YouTube videos effortlessly with VidFetch. Save your favorite videos for offline viewing, learning, or entertainment.
-</small></h2>
-<div className="frosted">
- <form action="/download" method="post">
-    <input type="text" name="url" placeholder="Enter YouTube video URL" required/>
-    <button type="submit">Download</button>
-    </form>
-    </div>
-<div className="unique">
-  <div className="unlimited">
-  <div className="con1">
-    <Image
-    src={Unlimited}
-    alt="Unlimited Downloads"
-    width={60}
-    quality={100}
-    />
-  </div>
-    <h3>Unlimited Downloads</h3>
-    <p>Download as many videos as you want, with no restrictions.</p>
-  </div>
-  <div className="fast">
-  <Image
-    src={Fast}
-    alt="Unlimited Downloads"
-    width={60}
-    quality={100}
-    />
-    <h3>Fast Downloads</h3>
-    <p>Download videos at high speed, without any buffering.</p>
-    </div>
-    <div className="free">
-    <Image
-    src={Free}
-    alt="Unlimited Downloads"
-    width={60}
-    quality={100}
-    />
-    <h3>Free to Use</h3>
-    <p>Download videos for free, with no hidden charges.</p>
-    </div>
-    <div className="accessibility">
-    <Image
-    src={Accessibility}
-    alt="Unlimited Downloads"
-    width={60}
-    quality={100}
-    />
-    <h3>Accessibility</h3>
-    <p>Download videos from any device, including mobile phones and tablets.</p>
-    </div>
-</div>
-</div>
+      <Navbar />
+      <div className="content">
+        <div className="logo">
+          <Image src={Logo} alt="VidFetch Logo" width={60} quality={100} />
+          <h1>VidFetch</h1>
+        </div>
+        <h1>VidFetch - YouTube Video Downloader</h1>
+        <h2>
+          <small>
+            Download YouTube videos effortlessly with VidFetch. Save your
+            favorite videos for offline viewing, learning, or entertainment.
+          </small>
+        </h2>
+        <div className="frosted">
+          <form action="/download" method="post">
+            <input
+              type="text"
+              name="url"
+              placeholder="Enter YouTube video URL"
+              required
+            />
+            <button type="submit">Download</button>
+          </form>
+        </div>
+        <div className="unique">
+          <div className="unlimited">
+            <div className="con1">
+              <Image
+                src={Unlimited}
+                alt="Unlimited Downloads"
+                width={60}
+                quality={100}
+              />
+            </div>
+            <h3>Unlimited Downloads</h3>
+            <p>Download as many videos as you want, with no restrictions.</p>
+          </div>
+          <div className="fast">
+            <Image
+              src={Fast}
+              alt="Unlimited Downloads"
+              width={60}
+              quality={100}
+            />
+            <h3>Fast Downloads</h3>
+            <p>Download videos at high speed, without any buffering.</p>
+          </div>
+          <div className="free">
+            <Image
+              src={Free}
+              alt="Unlimited Downloads"
+              width={60}
+              quality={100}
+            />
+            <h3>Free to Use</h3>
+            <p>Download videos for free, with no hidden charges.</p>
+          </div>
+          <div className="accessibility">
+            <Image
+              src={Accessibility}
+              alt="Unlimited Downloads"
+              width={60}
+              quality={100}
+            />
+            <h3>Accessibility</h3>
+            <p>
+              Download videos from any device, including mobile phones and
+              tablets.
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }

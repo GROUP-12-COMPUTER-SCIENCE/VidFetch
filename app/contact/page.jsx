16867519be2b3948ac97ed1a8437c92960816367
CHEGEBB <chegephil24@gmail.com>
2024-06-21 10:59:47 +0300

    new

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -10,19 +10,17 @@ export default function Contact() {
   return (
     <main className="contact">
       <Navbar />
-      <div className={styles.content}>
+      <div className="content-contact">
         <h1>Contact Us</h1>
         <h2>
           <small>We would love to hear from you! Fill out the form below to get in touch with us.</small>
         </h2>
-        <div className="frosted">
-          <form >
+          <form className="contact-form" >
             <input type="text" name="name" placeholder="Your Name" required />
             <input type="email" name="email" placeholder="Your Email" required />
             <textarea name="message" placeholder="Your Message" required></textarea>
             <button type="submit">Send Message</button>
           </form>
-        </div>
       </div>
     </main>
   );
